68454e7f2719a82bf96f9e652917ce35197ee713
argietxe <etchebarne.argitxu@gmail.com>
2024-04-15 15:38:39 +0200

    compare sunlight

--- models/mart/compare_sunlight_day.sql
+++ models/mart/compare_sunlight_day.sql
@@ -1,22 +1,25 @@
 SELECT 
 	city
-	,MIN(daytime) as shortest_day
-	,MAX(daytime) AS longest_day
-	,AVG(daytime) AS avg_daytime
+	,MIN(sunlight) as shortest_day
+	,MAX(sunlight) AS longest_day
+	,AVG(sunlight) AS avg_daytime
 FROM astro_forecast_day
 GROUP BY city
-ORDER BY shortest_day;
+ORDER BY shortest_day
 
 
+/* Let's find in what date this happens
 
--- i want to find out how to show the date for these values
+
 SELECT 
-    date, city, daytime
+    date, city, sunlight
 FROM 
     astro_forecast_day
 WHERE 
-    (city = 'Reykjavik' AND daytime IN ('04:06:00', '21:09:00'))
-    OR (city = 'Berlin' AND daytime IN ('07:39:00', '16:51:00'))
-    OR (city = 'Vancouver' AND daytime IN ('08:11:00', '16:15:00'))
-    OR (city = 'Biarritz' AND daytime IN ('08:57:00', '15:26:00'))
-    OR (city = 'Auckland' AND daytime IN ('09:37:00', '14:42:00'))
+    (city = 'Reykjavik' AND sunlight IN ('04:06:00', '21:09:00'))
+    OR (city = 'Berlin' AND sunlight IN ('07:39:00', '16:51:00'))
+    OR (city = 'Vancouver' AND sunlight IN ('08:11:00', '16:15:00'))
+    OR (city = 'Biarritz' AND sunlight IN ('08:57:00', '15:26:00'))
+    OR (city = 'Auckland' AND sunlight IN ('09:37:00', '14:42:00'));
+
+*/
